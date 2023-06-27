75438db92b64c75f6753b9e0ae3aa6b5d95e9422
Rajendra Bhausaheb Pacharne <69509779+Rajendra-pacharne@users.noreply.github.com>
2023-06-27 10:15:13 +0530

    26 june updates {Ancenter_Tree}

---
+++ src/com/treeprograms/MaxpathSum.java
@@ -0,0 +1,35 @@
+package com.treeprograms;
+
+public class MaxpathSum {
+
+    private static int max = Integer.MIN_VALUE;
+
+    private static int helper(Node root) {
+        if (root == null)
+            return 0;
+
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+
+        max = Math.max(max, root.val + left + right);
+
+        return root.val + Math.max(left, right);
+    }
+
+    public static int sumOfMaxPath(Node root) {
+        helper(root);
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(sumOfMaxPath(root));
+    }
+}
